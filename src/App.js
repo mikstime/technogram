096431ed199a77489afefffb87851a80032eecaa
MBtsky <9250499222@mail.ru>
2019-10-02 15:27:04 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+import Moments from './Moments'
+import Search from './Search'
+import Home from './Home'
+import Events from './Events'
+import Auth from './Auth'
+import CreateMoment from './CreateMoment'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+            <Route exact path='/moments'>
+                <Moments/>
+            </Route>
+            <Route exact path='/search'>
+                <Search/>
+            </Route>
+            <Route exact path='/home'>
+                <Home/>
+            </Route>
+            <Route exact path='/events'>
+                <Events/>
+            </Route>
+            <Route exact path='/auth'>
+                <Auth/>
+            </Route>
+            <Route exact path='/moments/create'>
+                <CreateMoment/>
+            </Route>
+            <Route>
+                Hello World
+            </Route>
+        </Switch>
+      </Router>
   );
 }
 
